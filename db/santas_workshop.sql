e0e8e17e4addd3ba3ad9a7417a2dcce309405d9e
Catherine Hall <catherine1992123@hotmail.com>
2019-12-14 12:50:49 +0000

    Begun to set up databases

---
+++ db/santas_workshop.sql
@@ -0,0 +1,19 @@
+DROP TABLE toys;
+DROP TABLE departments;
+
+CREATE TABLE departments
+(
+  id SERIAL primary key,
+  name VARCHAR(255) not null,
+  number_of_elves INT
+);
+
+CREATE TABLE toys
+(
+  id SERIAL primary key,
+  name VARCHAR(255) not null,
+  description VARCHAR(255),
+  stock_quantity INT;
+  buying_cost INT;
+  selling_price INT
+);
---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+require_relative( "../models/department.rb" )
+require_relative( "../models/toy.rb" )
+require("pry")
+
+
+department1 = Department.new({'name' => 'Soft Toys', 'number_of_elves' => 600})
+department2 = Department.new({'name' => 'Wooden Toys', 'number_of_elves' => 200})
+
+department1.save
+department2.save
+
+toy1 = Toy.new({'name' => 'Teddy Bear', 'description' => 'Traditional faun-coloured teddy bear', 'stock_quantity' => 7354, 'buying cost' => 10, 'selling price' => 5 })
+toy2 = Toy.new({'name' => 'Toy Cat', 'description' => 'Cuddly cat toy', 'stock_quantity' = 4869, 'buying_cost' => 8, 'selling_price' => 4 })
+
+
+toy1.save
+toy2.save
+
+
+binding.pry
+nil
