37d911e6045c60132b123a3eb21a5bf5f852c571
Catherine Hall <catherine1992123@hotmail.com>
2019-12-14 12:50:03 +0000

    Begun to set up models

---
+++ models/department.rb
@@ -0,0 +1,76 @@
+require_relative( '../db/sql_runner')
+
+class Department
+
+attr_accessor :id, :name, :number_of_elves
+
+  def initialize ( options )
+    @id = options['id'].to_i if options['id']
+    @name =  options ['name']
+    @number_of_elves = options ['number_of_elves']
+  end
+
+  def save()
+    sql = "INSERT INTO departments
+    (
+      name,
+      number_of_elves
+    )
+    VALUES
+    (
+      $1, $2
+    )
+    RETURNING id"
+    values = [@name, @number_of_elves]
+    results = SqlRunner.run(sql, values)
+    @id = results.first()['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM departments"
+    results = SqlRunner.run( sql )
+    return results.map { |department| Department.new( department ) }
+  end
+
+  def self.find( id )
+    sql = "SELECT * FROM departments
+    WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run( sql, values )
+    return Department.new( results.first )
+  end
+
+  def update()
+    sql = "UPDATE toys
+    SET
+    (
+      name,
+      number_of_elves
+      ) =
+      (
+        $1, $2,
+      )
+      WHERE id = $6"
+      values = [@name, @number_of_elves]
+      SqlRunner.run(sql, values)
+    end
+
+  def self.delete( id )
+    sql = "DELETE from departments
+    WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.delete_all
+    sql = "DELETE FROM departments"
+    SqlRunner.run( sql )
+  end
+
+
+
+
+
+
+
+end
---
+++ models/toy.rb
@@ -0,0 +1,79 @@
+require_relative( '../db/sql_runner' )
+
+class Toy
+
+  attr_accessor :id, :name, :description, :stock_quantity, :buying_cost, :selling_price
+
+  def initialize( options )
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @description = options['description']
+    @stock_quantity = options['stock_quantity']
+    @buying_cost = options['buying_cost']
+    @selling_price = options['selling_price']
+  end
+
+  def save()
+    sql = "INSERT INTO toys
+    (
+      name,
+      description,
+      stock_quantity,
+      buying_cost,
+      selling_price
+    )
+    VALUES
+    (
+      $1, $2, $3, $4, $5
+    )
+    RETURNING id"
+    values = [@name, @description, @stock_quantity, @buying_cost, @selling_price]
+    results = SqlRunner.run(sql, values)
+    @id = results.first()['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM toys"
+    results = SqlRunner.run( sql )
+    return results.map { |toy| Toy.new( toy ) }
+  end
+
+  def self.find( id )
+    sql = "SELECT * FROM toys
+    WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run( sql, values )
+    return Toy.new( results.first )
+  end
+
+  def update()
+    sql = "UPDATE toys
+    SET
+    (
+      name,
+      description,
+      stock_quantity,
+      buying_cost,
+      selling_price
+      ) =
+      (
+        $1, $2, $3, $4, $5
+      )
+      WHERE id = $6"
+      values = [@name, @description, @stock_quantity, @buying_cost, @selling_price]
+      SqlRunner.run(sql, values)
+    end
+
+    def self.delete( id )
+      sql = "DELETE * FROM toys
+      WHERE id = $1"
+      values = [@id]
+      SqlRunner.run(sql, values)
+
+      def self.delete_all
+        sql = "DELETE FROM toys"
+        SqlRunner.run( sql )
+      end
+
+
+    end
